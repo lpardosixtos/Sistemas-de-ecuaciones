44558a3c14e1b8aa75da849cfa97877bf5591a3e
Luis Fernando Pardo Sixtos <30965832+lpardosixtos@users.noreply.github.com>
2018-08-24 17:31:16 -0500

    Ejercicio Matrices Diagonales

--- main.cpp
+++ main.cpp
@@ -1,57 +1,38 @@
 #include <iostream>
+#include <fstream>
 #include "casostriviales.h"
 
 using namespace std;
 
-//esto es un cambio
 int main(){
 
-    char tipo;
-    cout << "De que tipo es tu matriz?\nD para diagonal, L para triangular inferior y U para triangular superior\n";
-    cin >> tipo;
-    cout << "¿Que dimension tiene tu matriz?\n";
     int sz;
-    cin >> sz;
-    cout << "Escribe los valores de b\n";
+    cout << "Ingrese el numero del caso a evaluar (4, 100, 500)" <<"\n";
+    string casesN;
+    cin >> casesN;
+    string fileNames="TAREA2_MATRICES//DIAG_MAT";
+    fileNames.append(casesN);
+    fileNames.append(".txt");
+    ifstream in_file;
+    in_file.open(fileNames);
+    in_file >> sz;
+    in_file >> sz;
+    double A[sz];
+    for(int i=0; i<sz; i++) in_file >> A[i];
+    fileNames[22]='V';
+    fileNames[23]='E';
+    fileNames[24]='C';
+    in_file.close();
+    in_file.clear();
+    in_file.open(fileNames);
+    
     double B[sz];
+    in_file >> sz;
+    for(int i=0; i<sz; i++) in_file>>B[i]; 
     double X[sz];
-    for(int i=0; i<sz; i++) cin >> B[i];
-    bool ind;
-    if(tipo=='D'){
-        cout << "Escribe los elementos de la diagonal\n";
-        double D[sz];
-        for(int i=0; i<sz; i++) cin >> D[i];
-        ind=solveDiag(D, B, X, sz);
-    }
-    else{
-        if(tipo=='L'){
-            cout << "Escribe los valores renglon por renglon parando en la diagonal \n";
-            double **L;
-            L=new double* [sz];
-            for(int i=0; i<sz; i++) L[i]=new double[sz];
-            for(int i=0; i<sz; i++){
-                for(int j=0; j<=i; j++) cin >> L[i][j];
-            }
-            ind=solveL(L, B, X, sz);
-        }
-        else{
-            cout << "Escribe los valores renglon por renglon empezando en la diagonal \n";
-            double** U;
-            U=new double* [sz];
-            for(int i=0; i<sz; i++) U[i]=new double[sz];
-            for(int i=0; i<sz; i++){
-                for(int j=i; j<sz; j++) cin >> U[i][j];
-            }
-            ind=solveU(U, B, X, sz);
-        }
-    }
-    if(!ind) cout << "El sistema no tiene solucion unica\n";
-    else{
-        cout << "La solucion es:\n";
-        cout << X[0];
-        for(int i=1; i<sz; i++) cout << " " << X[i];
-        cout << "\n";
-    }
-
+    solveDiag(A, B, X, sz);
+    cout << X[0] << " ";
+    for(int i=1; i<sz; i++) cout << " " << X[i];
+    cout << "\n";
     return 0;
 }
