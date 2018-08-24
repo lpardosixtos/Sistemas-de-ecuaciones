8d29b77111f6c210c781d8b876c7eb51a13a7ec9
Luis Fernando Pardo Sixtos <30965832+lpardosixtos@users.noreply.github.com>
2018-08-24 15:41:37 -0500

    Casos triviales header

---
+++ casostriviales.h
@@ -0,0 +1,10 @@
+#ifndef CASOSTRIVIALES_H
+#define CASOSTRIVIALES_H
+
+bool solveDiag(double* A, double* B, double*X, int sz);
+
+bool solveL(double* L[], double* B, double* X, int sz);
+
+bool solveU(double* U[], double* B, double* X, int sz);
+
+#endif // CASOSTRIVIALES_H
