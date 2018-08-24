2e194ab4c5102874024c3344d4867212caea7f37
Luis Fernando Pardo Sixtos <30965832+lpardosixtos@users.noreply.github.com>
2018-08-24 16:18:46 -0500

    Casos triviales

---
+++ casostriviales.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+bool solveDiag(double* A, double* B, double*X, int sz){
+    for(int i=0; i<sz; i++){
+        if(A[i]==0) return false;
+        X[i]=B[i]/A[i];
+    }
+    return true;
+}
+
+bool solveL(double** L, double* B, double* X, int sz){
+    if(L[0][0]==0)return false;
+    X[0]=B[0]/L[0][0];
+    for(int i=1; i<sz; i++){
+        if(L[i]==0) return false;
+        X[i]=B[i];
+        for(int j=0; j<i; j++){
+            double aux=L[i][j]*X[j];
+            X[i]-=aux;
+        }
+        X[i]/=L[i][i];
+    }
+    return true;
+}
+
+bool solveU(double** U, double* B, double* X, int sz){
+    sz--;
+    if(U[sz][sz]==0) return false;
+    X[sz]=B[sz]/U[sz][sz];
+    for(int i=sz-1; i>=0; i--){
+        if(U[i]==0) return false;
+        X[i]=B[i];
+        for(int j=sz; j>i; j--){
+            double aux=U[i][j]*X[j];
+            X[i]-=aux;
+        }
+        X[i]/=U[i][i];
+    }
+    return true;
+}
+
+
